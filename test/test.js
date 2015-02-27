f9763290582e752c134c1af792ad7ad36c5853f1
Tony Pujals <tony@subfuzion.com>
2015-02-27 12:10:07 -0800

    added test

---
+++ test/test.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+
+function notImplemented() {
+  throw new Error('not implemented');
+}
+
+
+describe('upload tests', function() {
+  
+  it ('should upload a file', function() {
+    notImplemented();
+  });
+
+});
+
+
+describe('download tests', function() {
+
+  it ('should download a file', function() {
+    notImplemented();
+  });
+
+});
+
+
